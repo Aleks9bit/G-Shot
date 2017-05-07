f2239a13bb818bd7e8ed926a6cbb7fea80bc97a4
alexey <alexey.bargin@gotoinc.eu>
2017-05-07 12:09:05 +0300

    add constant file

---
+++ G-Shot/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  G-Shot
+//
+//  Created by alexey on 5/6/17.
+//  Copyright © 2017 GoTo Inc. All rights reserved.
+//
+
+import UIKit
+
+let mainColor = UIColor(red:0.90, green:0.91, blue:0.91, alpha:1.00)
+let secondColor = UIColor(red:0.45, green:0.55, blue:0.00, alpha:1.00)
+let baseURL = "http://greenbook.co.il/?app=app_greenshot_login&"
+let userName = "id="
+let userPassword = "secure_num="
+let apiNum = "apiNum"
+let apiMsg = "apiMsg"
